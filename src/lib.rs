8508aa35bfceece388eb08475893190e4910a000
joefrost01 <joe_frost@mailcan.com>
2025-01-26 23:13:10 +0000

    Built site for gh-pages

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod config;
-pub mod from;
-pub mod to;
-pub mod transform;
-
-pub use config::Config;
-pub use from::{csv as from_csv, parquet as from_parquet, DataReader};
-pub use to::{csv as to_csv, parquet as to_parquet, DataWriter};
-pub use transform::{column_filter::ColumnFilter, Transform};
--- src/transform/mod.rs
+++
@@ -1,8 +0,0 @@
-use polars::prelude::*;
-use anyhow::Result;
-
-pub trait Transform {
-    fn transform(&self, df: DataFrame) -> Result<DataFrame>;
-}
-
-pub mod column_filter;
